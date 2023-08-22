d648248b111695cd746a6b52507806d31c87e2fe
mk812y <mk812y@gmail.com>
2023-08-22 16:03:04 +0200

    https://medium.com/@laxman.chilakala/textfield-in-swiftui-b19ed67e68c1

--- TextFieldSwiftUI/ContentView.swift
+++ TextFieldSwiftUI/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userName: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(Field.allCases, id: \.hashValue) { field in
+                TextField("xxx",
+                          text: $userName,
+                          prompt: Text(field.placeHolder).foregroundColor(.red.opacity(0.6)))//placeHolder color
+                    .submitLabel(.next) //?
+                    .padding()
+                    .background(Color(UIColor.yellow).cornerRadius(8.8))
+                    .shadow(radius: 8.8)
+                    .foregroundColor(.green) //text color
+                    .autocorrectionDisabled(true)
+                    .keyboardType(field.keyBoardType)
+            }
+            .padding(.horizontal)
         }
-        .padding()
+    }
+}
+
+extension ContentView {
+    //TextField Types
+    enum Field: Hashable, CaseIterable {
+        case name
+        case email
+        case password
+        case mobile
+        
+        var placeHolder: String {
+            switch self {
+            case .email: "Enter Email"
+            case .name: "Enter Username"
+            case .password: "Enter password"
+            case .mobile: "Enter mobile number"
+            }
+        }
+        
+        var keyBoardType: UIKeyboardType {
+            switch self {
+            case .email: .emailAddress
+            case .name, .password: .default
+            case .mobile: .asciiCapableNumberPad
+            }
+        }
     }
 }
 
